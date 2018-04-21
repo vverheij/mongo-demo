d6193ef1fb103b169cc448185e61dc4cfe1bd1f2
vverheij <v.verheij@gmail.com>
2018-04-21 23:49:00 +0200

    seconde

--- exercise/solution2.js
+++ exercise/solution2.js
@@ -13,13 +13,28 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
+// async function getCourses(){
+//     return await Course
+//         .find({isPublished: true, tags: 'backend'})
+//         .sort({name: 1})
+//         .select({name: 1 , isPublished: 1, backend: 1});
+// }
+
+// async function getCourses(){
+//     return await Course
+//         .find({isPublished: true, tags: {$in:['frontend','backend']}} )
+//         .sort('-price')
+//         .select('name author price');
+// }
+
 async function getCourses(){
     return await Course
-        .find({isPublished: true, tags: 'backend'})
-        .sort({name: 1})
-        .select({name: 1 , isPublished: 1, backend: 1});
+        .find({isPublished: true, tags: {$in:['frontend','backend']}, price: {$gt: 10} })
+        .or({price: {$gt: 10}},
+            {name: /.*by.*/i })
+        .sort('-price')
+        .select('name author price');
 }
-
 async function run(){
     const courses = await getCourses(); 
     console.log(courses);
